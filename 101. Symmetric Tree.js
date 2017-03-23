539f4c3346028663af99223cc17908788b4bca70
Fan Yang <1988.fan.yang@gmail.com>
2017-03-23 11:15:18 -0700

    ud

---
+++ 101. Symmetric Tree.js
@@ -0,0 +1,35 @@
+/*
+Given a binary tree, check whether it is a mirror of itself (ie, symmetric around its center).
+For example, this binary tree [1,2,2,3,4,4,3] is symmetric:
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following [1,2,2,null,3,null,3] is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+*/
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    if(!root) return true;
+    return helper(root.left, root.right);
+    function helper(nl, nr) {
+        if(!nl && !nr) return true;
+        if((!nl || !nr) || (nl.val !== nr.val)) return false;
+        return (nl.val === nr.val && helper(nl.left, nr.right) && helper(nl.right, nr.left));
+    }
+};
