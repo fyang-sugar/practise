f754550dff1e44b1f208da163a91751938ac3b83
Fan Yang <1988.fan.yang@gmail.com>
2018-02-21 15:11:13 -0800

    ud

---
+++ 532. K-diff Pairs in an Array.js
@@ -0,0 +1,34 @@
+/*
+Given an array of integers and an integer k, you need to find the number of unique k-diff pairs in the array. Here a k-diff pair is defined as an integer pair (i, j), where i and j are both numbers in the array and their absolute difference is k.
+Example 1:
+Input: [3, 1, 4, 1, 5], k = 2
+Output: 2
+Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
+Although we have two 1s in the input, we should only return the number of unique pairs.
+Example 2:
+Input:[1, 2, 3, 4, 5], k = 1
+Output: 4
+Explanation: There are four 1-diff pairs in the array, (1, 2), (2, 3), (3, 4) and (4, 5).
+Example 3:
+Input: [1, 3, 1, 5, 4], k = 0
+Output: 1
+Explanation: There is one 0-diff pair in the array, (1, 1).
+*/
+
+
+var findPairs = function(nums, k) {
+    if(k<0) return 0;
+    var i, count = 0, val, sub=nums, res=[], cur;
+    nums.sort((a,b) => b-a > 0);
+    for(i=0; i<nums.length; i++) {
+        cur = nums[i];
+        val = nums[i] - k;
+        sub.splice(i, 1);
+        if(sub.indexOf(val) > -1) {
+            res.push(val +''+cur);
+        }
+        sub.splice(i, 0, cur);
+    }
+    res= [...new Set(res)];
+    return res.length;
+};
