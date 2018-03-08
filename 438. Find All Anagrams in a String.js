5afe828e64bfcc7cf4c6c611153d873dbcce7f4a
Fan Yang <1988.fan.yang@gmail.com>
2018-03-08 11:00:26 -0800

    ud

--- 438. Find All Anagrams in a String.js
+++ 438. Find All Anagrams in a String.js
@@ -19,42 +19,31 @@ Explanation:
 The substring with start index = 0 is "ab", which is an anagram of "ab".
 The substring with start index = 1 is "ba", which is an anagram of "ab".
 The substring with start index = 2 is "ab", which is an anagram of "ab".
+
+how to check if two arrays are the same, do a delta array and get the sum of each abs(element), if sum=0. the arrays are the same
 */
 
 var findAnagrams = function(s, p) {
-    let map1 = {}, map2 = {};
-    var i=0, res=[];
-    if(p.length > s.length || (p.length === s.length && p !== s)) return [];
-    if(p.length === s.length && p === s) return [0];
-    for(i=0; i<p.length; i++) {
-        map1[p.charAt(i)] = map1[p.charAt(i)] ?  map1[p.charAt(i)]+1 :1;
-        map2[s.charAt(i)] = map2[s.charAt(i)] ?  map2[s.charAt(i)]+1 :1;
+    var delta = {}, res = [], i;
+    for( i=0; i<p.length; i++) {
+        delta[p[i]] = delta[p[i]] ? delta[p[i]]-1 : -1;
+        delta[s[i]] = delta[s[i]] ? delta[s[i]]+1 : 1;
     }
-     if(isEqual(map1, map2)) {
-            res.push(0);
-     }
-    
-    for(i=p.length; i<s.length; i++) {
-        // the left one should be forgotten
-        map2[s.charAt(i- p.length)] --; 
-        
-        //increase map2 by one step further
-        map2[s.charAt(i)] = map2[s.charAt(i)] ?  map2[s.charAt(i)]+1 :1;
-        
-        //Compare map1 and map2, if equal, add i-p.length
-        if(isEqual(map1, map2)) {
-            res.push(i - p.length +1);
-        }
+    if(isAnagram(delta))  res.push(0);
+    for( i=p.length; i<s.length; i++) {
+        delta[s[i]] = delta[s[i]] ? delta[s[i]]+1 : 1;
+         delta[s[i-p.length]] =  delta[s[i-p.length]]-1;
+          if(isAnagram(delta)) {
+              res.push(i- p.length +1); 
+          }
     }
     return res;
 };
 
-var isEqual = function(map1, map2) {
-    var key;
-   for( key in map1) {
-       if(map2[key] !== map1[key]) {
-           return false;
-       }
-   }
-    return true;
+var isAnagram = function(delta) {
+    var sum = 0;
+    for(var key in delta) {
+        sum+= Math.abs(delta[key]);
+    }
+    return sum === 0;
 };
