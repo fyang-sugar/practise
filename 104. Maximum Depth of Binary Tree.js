ba415f94851745033f24353bfdb97f1e0e0a8e5e
Fan Yang <1988.fan.yang@gmail.com>
2018-03-11 11:22:09 -0700

    ud

--- 104. Maximum Depth of Binary Tree.js
+++ 104. Maximum Depth of Binary Tree.js
@@ -10,8 +10,16 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-    if(!root) return 0;
-    var l = maxDepth(root.left);
-    var r = maxDepth(root.right);
-    return Math.max(l, r)+1;
+    if(root === null) return 0;
+    var count = 0;
+    return Depth(root, count);
+};
+
+var Depth = function(node, count) {
+    if(node !== null && node.left === null && node.right === null ) {
+        return count+1;
+    }
+    var left = (node.left !== null) ? Depth(node.left, count) : count;
+    var right =(node.right !== null) ? Depth(node.right, count) : count;
+    return Math.max(left, right) +1; 
 };
