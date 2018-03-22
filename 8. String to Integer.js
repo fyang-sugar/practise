353166243aefe92974085e1d6da8977e0d5d521a
Fan Yang <1988.fan.yang@gmail.com>
2018-03-21 23:15:32 -0700

    ud

--- 8. String to Integer.js
+++ 8. String to Integer.js
@@ -1,20 +1,30 @@
 // Implement atoi to convert a string to an integer.
-
 /**
  * @param {string} str
  * @return {number}
- * Integer max = 2147483647, min = -2147483648;
+  Integer max = 2147483647, min = -2147483648;
  */
 var myAtoi = function(str) {
-   var Int_Max = 2147483647, Int_Min = -2147483648;
-   var res= parseInt(str);
-   if(res > Int_Max) {
-       return Int_Max;
-   }
-   else if(res < Int_Min) {
-       return Int_Min;
-   }
-   else {
-       return isNaN(res) ? 0 : res;
-   }
+    var flag = true;
+    str = str.trim();
+    if(str.length === 0) return 0;
+    var i=0, sum=0;
+    if(str[0] === '+' || str[0] === '-')  {
+         flag = str[0] === '-' ? false : true;
+         i++;
+    }
+    if(isNaN(str[i]))  
+          return 0;
+    
+    while(str[i] <= '9' && str[i] >= '0' && i< str.length) {
+          sum = sum * 10 + parseInt(str[i++]);
+    }
+       
+    if(flag) {
+        if(sum > 2147483647)  return 2147483647;
+        else return sum;
+    } else {
+        if(sum > 2147483648)  return -2147483648;
+        else return -1*sum;
+    } 
 };
