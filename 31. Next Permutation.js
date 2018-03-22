f1faf00b401164057723d2eb5d8832f6578a1303
Fan Yang <1988.fan.yang@gmail.com>
2018-03-22 16:00:08 -0700

    ud

--- 31. Next Permutation.js
+++ 31. Next Permutation.js
@@ -1,23 +1,13 @@
-/*
-Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
-If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
-The replacement must be in-place, do not allocate extra memory.
-Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
-1,2,3 → 1,3,2
-3,2,1 → 1,2,3
-1,1,5 → 1,5,1
-*/
-
-
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  * check if all nums[i] > nums[i+1]  if so, resort the array and return the result
    otherwise, find the i where nums[i] > nums[i+1], point  = i, find the min larger number then nums[i] in the array and swap it with the nums[point],
-   687321   - 786321  sort 
+   687321   - 786321  sort 86321, found out already sorted, swap first with last, ....
+   
  */
 var nextPermutation = function(nums) {
-    var point, st= [], tobeSwapped, swap, temp, sub;
+    var point, st= [], tobeSwapped, swap, temp;
     for(var i=nums.length-1; i>=1; i--) {
         if(nums[i] > nums[i-1]) {
             point = i-1;
@@ -40,14 +30,14 @@ var nextPermutation = function(nums) {
     nums[point] = nums[tobeSwapped];
     nums[tobeSwapped] = temp;
     
-    sub = nums.slice(point+1, nums.length).sort((a, b) => a-b);
-    var i= nums.length-1;
-    while(i > point) {
-        nums.pop();
-        i--;
-    }
-    while(sub.length > 0) {
-        nums.push(sub.shift());
+    var start = point+1;
+    var end = nums.length-1;
+    while(start < end) {
+        temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+        start ++;
+        end --;
     }
     return;
 };
