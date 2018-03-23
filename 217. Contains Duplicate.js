fbd27d74942fe64555eef75eb2d96acc2d5626a3
Fan Yang <1988.fan.yang@gmail.com>
2018-03-22 23:02:50 -0700

    ud

--- 217. Contains Duplicate.js
+++ 217. Contains Duplicate.js
@@ -2,18 +2,19 @@
 Given an array of integers, find if the array contains any duplicates. 
 Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
 */
+
+
 /**
  * @param {number[]} nums
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    var i;
-    nums.sort((a, b)=> {
-        return a-b;
-    });
-    
-    for(i=1; i<nums.length; i++) {
-        if(nums[i] === nums[i-1]) {
+    var map = {};
+    for(var i=0; i<nums.length; i++) {
+        map[nums[i]] = map[nums[i]] ? map[nums[i]]+1 : 1;
+    }
+    for(var key in map) {
+        if(map[key] >=2) {
             return true;
         }
     }
