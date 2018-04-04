409ea1864337d3b8ead396aebc380ab9cca86533
Fan Yang <1988.fan.yang@gmail.com>
2018-04-03 23:53:31 -0700

    ud

---
+++ 775. Global and Local Inversions.js
@@ -0,0 +1,35 @@
+/*
+We have some permutation A of [0, 1, ..., N - 1], where N is the length of A.
+The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
+The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].
+Return true if and only if the number of global inversions is equal to the number of local inversions.
+Example 1:
+Input: A = [1,0,2]
+Output: true
+Explanation: There is 1 global inversion, and 1 local inversion.
+Example 2:
+Input: A = [1,2,0]
+Output: false
+Explanation: There are 2 global inversions, and 1 local inversion.
+
+Note:
+A will be a permutation of [0, 1, ..., A.length - 1].
+A will have length in range [1, 5000].
+The time limit for this problem has been reduced.
+*/
+
+/**
+ * @param {number[]} A
+ * @return {boolean}
+ The original order should be [0, 1, 2, 3, 4…], the number i should be on the position i. We just check the offset of each number, if the absolute value is larger than 1, means the number of global inversion must be bigger than local inversion, because a local inversion is a global inversion, but a global one may not be local.
+
+e.g: [2, 0, 1] false  offset of 2 is 2 > 1
+ */
+var isIdealPermutation = function(A) {
+    for(var i = 0; i<A.length; i++) {
+        if(Math.abs(A[i] - i) > 1) {
+            return false;
+        }
+    }
+    return true;
+};
