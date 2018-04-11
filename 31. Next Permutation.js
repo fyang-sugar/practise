7b7b585a803fe15bf923e4c80c6d7248bcef0fea
Fan Yang <1988.fan.yang@gmail.com>
2018-04-11 12:41:02 -0700

    ud

--- 31. Next Permutation.js
+++ 31. Next Permutation.js
@@ -1,9 +1,22 @@
+/*
+Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
+The replacement must be in-place, do not allocate extra memory.
+Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
+1,2,3 → 1,3,2
+3,2,1 → 1,2,3
+1,1,5 → 1,5,1
+*/
+
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  * check if all nums[i] > nums[i+1]  if so, resort the array and return the result
    otherwise, find the i where nums[i] > nums[i+1], point  = i, find the min larger number then nums[i] in the array and swap it with the nums[point],
-   687321   - 786321  sort 86321, found out already sorted, swap first with last, ....
+   687321  find 6 is the place where nums[i+1] > nums[i], mark it as point
+   from right to left, find the first larger number then 6, which is 7
+   swap 6 with 7, changed to 786321 
+   then from point to end reverse 86321
    
  */
 var nextPermutation = function(nums) {
