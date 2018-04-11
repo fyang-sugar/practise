bf7c5b4a4f42e4873b5858884aeea08f39f5dec6
Fan Yang <1988.fan.yang@gmail.com>
2018-04-11 16:31:05 -0700

    ud

--- 459. Repeated Substring Pattern.js
+++ 459. Repeated Substring Pattern.js
@@ -16,20 +16,21 @@ Explanation: It's the substring "abc" four times. (And the substring "abcabc" tw
 /**
  * @param {string} s
  * @return {boolean}
+  // e.g: ababab
+         aaaaaaaa
  split s into 2, just work on first half, if no subtring with 1~first-half length can add up the the orignal string, then return false.
  */
+
 var repeatedSubstringPattern = function(s) {
-   var i, sub, temp;
-   for(i=parseInt(s.length /2); i>=1; i--) {
-       sub = s.substr(0, i);
-       temp = sub;
+    var sub = s.substr(0, parseInt(s.length /2));
+    var len = sub.length;
+    for(i=0; i<len; i++) {
+       var temp = sub;
        while(temp.length < s.length) {
            temp += sub;
-           if(temp === s) {
-               return true;
-           }
+           if(temp === s)  return true;
        }
-   }
+       sub = sub.substr(0, sub.length-1);
+    }
     return false;
 };
-
