e56c073f7c8992b0bde9fecfe80cb5357bb00e23
Fan Yang <1988.fan.yang@gmail.com>
2018-04-17 13:42:28 -0700

    ud

---
+++ 133. Clone Graph.js
@@ -0,0 +1,54 @@
+/*
+Clone an undirected graph. Each node in the graph contains a label and a list of its neighbors.
+OJ's undirected graph serialization:
+Nodes are labeled uniquely.
+We use # as a separator for each node, and , as a separator for node label and each neighbor of the node.
+As an example, consider the serialized graph {0,1,2#1,2#2,2}.
+The graph has a total of three nodes, and therefore contains three parts as separated by #.
+First node is labeled as 0. Connect node 0 to both nodes 1 and 2.
+Second node is labeled as 1. Connect node 1 to node 2.
+Third node is labeled as 2. Connect node 2 to node 2 (itself), thus forming a self-cycle.
+Visually, the graph looks like the following:
+       1
+      / \
+     /   \
+    0 --- 2
+         / \
+         \_/
+*/
+
+/**
+ * Definition for undirected graph.
+ * function UndirectedGraphNode(label) {
+ *     this.label = label;
+ *     this.neighbors = [];   // Array of UndirectedGraphNode
+ * }
+ */
+
+/**
+ * @param {UndirectedGraphNode} graph
+ * @return {UndirectedGraphNode}
+ */
+var cloneGraph = function(graph) {
+    if(!graph)  return graph;
+    // We use a map to store the original node map to the copy node.
+    var map = new Map();
+    var copyHead = new UndirectedGraphNode(graph.label);
+    var st = [graph];
+    map.set(graph, copyHead);
+    while(st.length >0) {
+        var curNode = st.shift();
+        var curCopy = map.get(curNode);
+        for(var neighbor of curNode.neighbors) {
+            if(map.get(neighbor)) {     // this neighbor already visited.
+                curCopy.neighbors.push(map.get(neighbor));
+            } else {
+                var neighborCopy = new UndirectedGraphNode(neighbor.label);
+                map.set(neighbor, neighborCopy);
+                curCopy.neighbors.push(neighborCopy);
+                st.push(neighbor);
+            }
+        }
+    }
+    return copyHead;
+};
